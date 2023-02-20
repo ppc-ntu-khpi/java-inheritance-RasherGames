90ac090ee07d0b0abaff3acf4d34b3ff2d054b8e
Nikolay Tkachenko <110029945+RasherGames@users.noreply.github.com>
2023-02-20 22:17:32 +0200

    Add files via upload

---
+++ src/domain/Elephant.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Elephant extends Animal {
+
+     public Elephant(String name, int age) {
+        super(name, age);
+    }
+
+    public void trumpet() {
+        System.out.println("I am trumpeting");
+    }
+         
+}
